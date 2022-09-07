f287f033a441e4a4a141725a37011735bf77563f
Sansone11 <sansonemirco@gmail.com>
2022-09-07 15:07:49 +0200

    text-edit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <MainHeader />
+  <MainContent />
+</template>
+
+<script>
+import MainHeader from './MainHeader.vue';
+import MainContent from './MainContent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader,
+    MainContent,
+   
+}
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="wrapper">
+        <div class="row">
+            <div>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+  </b-card>
+</div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    // name: 'MainContent',
+    // props: {
+    //     msg: String
+    // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
